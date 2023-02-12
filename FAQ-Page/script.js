a08621cf1b59b39556ad6b2353fa7803013e4a26
sageerkv <sageerkv93@gmail.com>
2023-02-12 16:14:12 +0530

    final commit

--- FAQ-Page/script.js
+++ FAQ-Page/script.js
@@ -1,10 +1,28 @@
 const questions = document.querySelectorAll('.question');
 
 questions.forEach((question) => {
-  const btn = question.querySelector('.faq-plus');
+  const btn = question.querySelector('#faq-plus,#faq-pluss');
   const answer = question.querySelector('.answer');
 
   btn.addEventListener('click', () => {
     answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
   });
 });
+
+function changeIcon() {
+  var faq = document.getElementById('faq-plus');
+  if (faq.src.match("minus")) {
+    faq.src = "img/plus.png";
+  } else {
+    faq.src = "img/minus.png";
+  }
+}
+
+function changeIcons() {
+  var faq = document.getElementById('faq-pluss');
+  if (faq.src.match("minus")) {
+    faq.src = "img/plus.png";
+  } else {
+    faq.src = "img/minus.png";
+  }
+}
